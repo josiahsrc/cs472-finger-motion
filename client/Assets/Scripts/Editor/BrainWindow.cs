ea197020a9ecb0c0261823e137cd1f44ca53452b
Josiah <contact@josiahsaunders.com>
2020-11-02 16:27:45 -0700

    run animation to gather data

--- client/Assets/Scripts/Editor/BrainWindow.cs
+++ client/Assets/Scripts/Editor/BrainWindow.cs
@@ -1,12 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEditor;
-using System;
+using Unity.EditorCoroutines.Editor;
 
 public class BrainWindow : EditorWindow
 {
-    [SerializeField] private TrainingConfig _trainingConfig = new TrainingConfig();
+    [SerializeField] private FitConfig _fitConfig = new FitConfig();
 
-    private TrainingSession _trainingSession = new TrainingSession();
+    private FitSession _fitSession = new FitSession();
     private Vector2 _windowScrollPos = Vector2.zero;
 
     [MenuItem("Window/App/Brain")]
@@ -29,49 +32,153 @@ public class BrainWindow : EditorWindow
     private void drawTrainingGUI()
     {
         GUILayout.Label("Training", EditorStyles.boldLabel);
-        _trainingConfig.body = EditorGUILayout.ObjectField("Body", _trainingConfig.body, typeof(Body), true) as Body;
-        _trainingConfig.clip = EditorGUILayout.ObjectField("Clip", _trainingConfig.clip, typeof(AnimationClip), false) as AnimationClip;
-        _trainingConfig.duration = EditorGUILayout.FloatField("Duration", _trainingConfig.duration);
-        _trainingConfig.startDelay = EditorGUILayout.FloatField("Start Delay", _trainingConfig.startDelay);
-        _trainingConfig.speed = EditorGUILayout.FloatField("Speed", _trainingConfig.speed);
+        _fitConfig.body = EditorGUILayout.ObjectField("Body", _fitConfig.body, typeof(Body), true) as Body;
+        _fitConfig.clip = EditorGUILayout.ObjectField("Clip", _fitConfig.clip, typeof(AnimationClip), false) as AnimationClip;
+        _fitConfig.duration = EditorGUILayout.FloatField("Duration", _fitConfig.duration);
+        _fitConfig.startDelay = EditorGUILayout.FloatField("Start Delay", _fitConfig.startDelay);
+        _fitConfig.snapshotInterval = EditorGUILayout.FloatField("Snapshot Interval", _fitConfig.snapshotInterval);
+        _fitConfig.speed = EditorGUILayout.FloatField("Speed", _fitConfig.speed);
 
         EditorGUILayout.Space();
         {
+            var remainingTime = TimeSpan.FromSeconds(_fitSession.remainingTime).ToString("mm':'ss':'ff");
+            var status = _fitSession.status.Length == 0 ? "N/A" : _fitSession.status;
             EditorGUILayout.HelpBox(
                 $"CURRENT SESSION INFO\n" +
-                $"running:\t\t{_trainingSession.isRunning}\n" +
-                $"time:\t\t{TimeSpan.FromSeconds(_trainingSession.t).ToString("mm':'ss':'ff")}\n" +
-                $"percent complete:\t{Mathf.Clamp01(_trainingSession.t / _trainingConfig.duration) * 100}%",
+                $"running:\t{_fitSession.isRunning}\n" +
+                $"time:\t{remainingTime}\n" +
+                $"status:\t{status}",
                 MessageType.Info
             );
         }
 
-        EditorGUILayout.Space();
-        if (GUILayout.Button("Fit (Override)"))
-        {
+        var prevGUIEnabled = GUI.enabled;
 
+        GUI.enabled = !_fitSession.isRunning;
+        EditorGUILayout.Space();
+        if (GUILayout.Button("Fit (Overwrite)"))
+        {
+            fit_begin(true);
         }
 
         if (GUILayout.Button("Fit (Append)"))
         {
-
+            fit_begin(false);
         }
+
+        GUI.enabled = _fitSession.isRunning;
+        EditorGUILayout.Space();
+        if (GUILayout.Button("Stop"))
+        {
+            fit_stop();
+        }
+
+        GUI.enabled = prevGUIEnabled;
+    }
+
+    private void fit_begin(bool overwrite)
+    {
+        Debug.Assert(_fitConfig.body != null);
+        Debug.Assert(_fitConfig.clip != null);
+        Debug.Assert(!_fitSession.isRunning);
+
+        _fitSession.remainingTime = 0;
+        _fitSession.status = "";
+        _fitSession.isRunning = true;
+        _fitSession.routine = EditorCoroutineUtility.StartCoroutine(fit_routine(), this);
+        Repaint();
+    }
+
+    private void fit_stop()
+    {
+        if (_fitSession.routine != null)
+        {
+            EditorCoroutineUtility.StopCoroutine(_fitSession.routine);
+        }
+
+        _fitSession.remainingTime = 0;
+        _fitSession.status = "";
+        _fitSession.isRunning = false;
+        _fitSession.routine = null;
+        Repaint();
+    }
+
+    private IEnumerator fit_routine()
+    {
+        var threshStart = _fitConfig.startDelay;
+        var threshGatherData = _fitConfig.duration + threshStart;
+        var durationTotal = threshGatherData;
+
+        var futureConfig = new FutureRoutine.Config();
+        futureConfig.duration = _fitConfig.duration;
+
+        AnimationMode.StartAnimationMode();
+        yield return FutureRoutine.run(futureConfig, (input) =>
+        {
+            var output = new FutureRoutine.Output();
+
+            void updateAnimationClip()
+            {
+                if (EditorApplication.isPlaying || !AnimationMode.InAnimationMode())
+                {
+                    Debug.LogError("Unable to fit.");
+                    output.stop = true;
+                }
+
+                AnimationMode.BeginSampling();
+                AnimationMode.SampleAnimationClip(_fitConfig.body.gameObject, _fitConfig.clip, input.elapsedTime % _fitConfig.clip.length);
+                AnimationMode.EndSampling();
+
+                SceneView.RepaintAll();
+                Repaint();
+            }
+
+            if (input.elapsedTime < threshStart)
+            {
+                _fitSession.remainingTime = threshStart - input.elapsedTime;
+                _fitSession.status = "Starting";
+                updateAnimationClip();
+            }
+            else if (input.elapsedTime < threshGatherData)
+            {
+                _fitSession.remainingTime = threshGatherData - input.elapsedTime;
+                _fitSession.status = "Gathering data";
+                updateAnimationClip();
+            }
+
+            return output;
+        });
+
+        AnimationMode.StopAnimationMode();
+        SceneView.RepaintAll();
+        Repaint();
+        fit_stop();
+    }
+
+    private IEnumerator fit_captureRoutine()
+    {
+        var futureConfig = new FutureRoutine.Config();
+        futureConfig.duration = 1;
+        yield return FutureRoutine.run(futureConfig);
     }
 
     [Serializable]
-    private class TrainingConfig
+    private class FitConfig
     {
         public Body body = null;
         public AnimationClip clip = null;
         public float duration = 10;
         public float startDelay = 3;
+        public float snapshotInterval = 0.1f;
         public float speed = 1;
     }
 
     [Serializable]
-    private class TrainingSession
+    private class FitSession
     {
         public bool isRunning = false;
-        public float t = 0.0f;
+        public float remainingTime = 0.0f;
+        public string status = "";
+        public EditorCoroutine routine = null;
     }
 }
---
+++ client/Assets/Scripts/Editor/FutureRoutine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class FutureRoutine
+{
+    public delegate Output TickCallback(Input input);
+
+    public static IEnumerator run(Config config, TickCallback onTick = null)
+    {
+        double startTime = EditorApplication.timeSinceStartup;
+        double elapsedTime = 0;
+
+        while (elapsedTime < (config.duration ?? double.PositiveInfinity))
+        {
+            elapsedTime = EditorApplication.timeSinceStartup - startTime;
+
+            var input = new Input();
+            input.elapsedTime = (float)elapsedTime;
+            input.remainingTime = config.duration != null ? (float)elapsedTime - config.duration ?? 0 : float.NaN;
+
+            var output = onTick?.Invoke(input);
+            if ((output?.stop ?? false))
+            {
+                break;
+            }
+
+            yield return null;
+        }
+    }
+
+    public struct Config
+    {
+        public float? duration;
+    }
+
+    public struct Input
+    {
+        public float elapsedTime;
+        public float remainingTime;
+    }
+
+    public struct Output
+    {
+        public bool? stop;
+    }
+}
