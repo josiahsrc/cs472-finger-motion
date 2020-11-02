ed53ba5ffbedb4c972bc0c62b9e0b0370e29c45d
Josiah <contact@josiahsaunders.com>
2020-11-02 11:32:04 -0700

    added brain fit/score/predict skeletons

---
+++ client/Assets/Scripts/Runtime/BodyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyController : MonoBehaviour
+{
+    [SerializeField] private BodyVectorTransform _transforms = null;
+
+    public BodyVectorFloat takeSnapshot()
+    {
+        var snapshot = new BodyVectorFloat();
+
+        for (int i = 0; i < snapshot.length; ++i)
+        {
+            snapshot[i] = _transforms[i].localEulerAngles.z;
+        }
+
+        return snapshot;
+    }
+}
---
+++ client/Assets/Scripts/Runtime/Brain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="Brain", menuName="App/Brain")]
+public class Brain : ScriptableObject
+{
+    BrainFitResponse fit(BrainFitRequest request)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    BrainPredictResponse predict(BrainPredictRequest request)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    BrainScoreResponse score(BrainScoreRequest request)
+    {
+        throw new System.NotImplementedException();
+    }
+}
