6050c48baeec9bd1742411f264b234a4965860b1
Josiah <contact@josiahsaunders.com>
2020-11-07 16:14:34 -0700

    cleanup some assets

--- brain/lib/communication.py
+++
@@ -1,9 +0,0 @@
-# TODO: import jsonpickle
-
-class Communicator:
-    def __init__():
-        pass
-
-class LogResponse:
-    def __init__():
-        pass
--- brain/lib/main.py
+++
@@ -1,1 +0,0 @@
-print('hello world from the brain server!')
