eb8cca59f17efff47c36410fb9cc7b0b6e64df89
Robinson Paz <robinson.paz.jesus@gmail.com>
2020-11-09 12:54:01 -0700

    add comment

--- brain/main.py
+++ brain/main.py
@@ -8,5 +8,6 @@ from arff import Arff
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(tf.__version__)
+    #update
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
