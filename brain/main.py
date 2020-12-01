700b23c177e61d82585528c82a0f078780277d7f
Josiah <contact@josiahsaunders.com>
2020-12-01 16:38:21 -0700

    predictions working (sort of)

--- brain/main.py
+++ brain/main.py
@@ -16,7 +16,7 @@ import data_utils as dutils
 from multiprocessing import Process, Queue
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import normalize, Normalizer
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 
@@ -29,66 +29,9 @@ class DevContext:
         self.n_features = None
         self.n_labels = None
         self.imputer = None
+        self.normalizer = None
         pass
 
-
-# APPEND INSTANCE USING DELTAS
-# def appendInstance(reqTuple):
-#   #appends to a certian training data file
-#   request = reqTuple[0]
-#   prevRequest = reqTuple[1]
-#   points = reqTuple[2]
-#   prevPoints = reqTuple[3]
-
-#   #grab the filepath to write to,
-#   filepath = request['csvPath']
-
-#   #calculate delta angles
-#   angles = np.copy(request['outputs'])
-#   prev_angles = np.copy(prevRequest['outputs'])
-#   delta_angles = angles - prev_angles
-
-#   #calculate delta points
-#   delta_points = []
-#   for point1, point2 in zip(points, prevPoints):
-#     if point1 is None or point2 is None:
-#       delta_points.append(None)
-
-#     else:
-#       point1 = np.array(point1)
-#       point2 = np.array(point2)
-#       delta = point1 - point2
-#       delta_points.append(delta)
-
-#   # Clean up delta_points
-#   new_points = []
-#   for point in delta_points:
-#     if point is not None:
-#       new_points.append(point[0])
-#       new_points.append(point[1])
-#     else:
-#       new_points.append(None)
-#       new_points.append(None)
-
-#   # Combining into one row
-#   final_row = np.concatenate((new_points, delta_angles), axis=0)
-
-#   #now they need to be writen to the file
-#   # print('delta angles:', delta_angles)
-#   # print('delta positions:', delta_points)
-
-#   # Make sure the specified folder exists
-#   dir_name = os.path.dirname(filepath)
-#   if not os.path.exists(dir_name):
-#     os.makedirs(dir_name)
-
-#   with open(filepath, 'a+') as fd:
-#     writer = csv.writer(fd)
-#     writer.writerow(final_row)
-
-#   #return the response
-#   return {'type': 'append_instance',
-#           'message': 'instance appended to {}'.format(filepath)}
 
 # APPEND INSTANCE USING RAW POINTS
 def appendInstance(reqTuple):
@@ -125,109 +68,96 @@ def appendInstance(reqTuple):
     return {'type': 'append_instance',
             'message': 'instance appended to {}'.format(filepath)}
 
+
 def saveModel(reqTuple):
     # saves a pre-trained network
     pass
+
 
 def loadModel():
     # loads a pre-trained network
     pass
 
+
 def predict(reqTuple, context: DevContext):
+    # The issue here is that the inputs ARENT normalized
+
     curr_points = reqTuple[2]
     prev_points = reqTuple[3]
-    
+
     if prev_points is None or curr_points is None:
         return
-    
+
     prev_pt_grn = None if prev_points[0] is None else np.array(prev_points[0])
     prev_pt_blu = None if prev_points[1] is None else np.array(prev_points[1])
     prev_pt_prp = None if prev_points[2] is None else np.array(prev_points[2])
     prev_pt_org = None if prev_points[3] is None else np.array(prev_points[3])
     prev_pt_pnk = None if prev_points[4] is None else np.array(prev_points[4])
-    
+
     pt_grn = None if curr_points[0] is None else np.array(curr_points[0])
     pt_blu = None if curr_points[1] is None else np.array(curr_points[1])
     pt_prp = None if curr_points[2] is None else np.array(curr_points[2])
     pt_org = None if curr_points[3] is None else np.array(curr_points[3])
     pt_pnk = None if curr_points[4] is None else np.array(curr_points[4])
-    
+
     # Directional finger distance vectors
     diff_grn = None if pt_grn is None or prev_pt_grn is None else pt_grn - prev_pt_grn
     diff_blu = None if pt_blu is None or prev_pt_blu is None else pt_blu - prev_pt_blu
     diff_prp = None if pt_prp is None or prev_pt_prp is None else pt_prp - prev_pt_prp
     diff_org = None if pt_org is None or prev_pt_org is None else pt_org - prev_pt_org
     diff_pnk = None if pt_pnk is None or prev_pt_pnk is None else pt_pnk - prev_pt_pnk
-    
+
     # Input into the network
     input = np.array([
-        # Deltas (Tensorflow will normalize these automatically)
-        None if diff_grn is None else diff_grn[0], 
+        # Deltas
+        None if diff_grn is None else diff_grn[0],
         None if diff_grn is None else diff_grn[1],
-        
-        None if diff_blu is None else diff_blu[0], 
+
+        None if diff_blu is None else diff_blu[0],
         None if diff_blu is None else diff_blu[1],
-        
-        None if diff_prp is None else diff_prp[0], 
+
+        None if diff_prp is None else diff_prp[0],
         None if diff_prp is None else diff_prp[1],
-        
-        None if diff_org is None else diff_org[0], 
+
+        None if diff_org is None else diff_org[0],
         None if diff_org is None else diff_org[1],
-        
-        None if diff_pnk is None else diff_pnk[0], 
+
+        None if diff_pnk is None else diff_pnk[0],
         None if diff_pnk is None else diff_pnk[1],
-        
-        # Points (Tensorflow will normalize these automatically)
-        None if pt_grn is None else pt_grn[0], 
+
+        # Points
+        None if pt_grn is None else pt_grn[0],
         None if pt_grn is None else pt_grn[1],
-        
-        None if pt_blu is None else pt_blu[0], 
+
+        None if pt_blu is None else pt_blu[0],
         None if pt_blu is None else pt_blu[1],
-        
-        None if pt_prp is None else pt_prp[0], 
+
+        None if pt_prp is None else pt_prp[0],
         None if pt_prp is None else pt_prp[1],
-        
-        None if pt_org is None else pt_org[0], 
+
+        None if pt_org is None else pt_org[0],
         None if pt_org is None else pt_org[1],
-        
+
         None if pt_pnk is None else pt_pnk[0],
         None if pt_pnk is None else pt_pnk[1],
     ]).reshape(1, -1)
-    
+
     # Impute missing values
-    imputed_input = context.imputer.transform(input)
+    input = context.imputer.transform(input)
     
+    # Normalize the data based on past findings
+    input = context.normalizer.transform(input)
+
     # Predict output
-    output = context.model.predict(imputed_input).reshape(-1).tolist()
+    output = context.model.predict(input).reshape(-1).tolist()
 
     # Return the prediction
     return {'type': 'predict',
-            'prediction': output }
+            'prediction': output}
 
 
 def fit(reqTuple, context):
     pass
-#   request = reqTuple[0]
-#   filepaths = request['csvPaths']
-#   print(f'fit: filepaths={filepaths}')
-
-#   try:
-#     for fpath in filepaths:
-
-#       df = pandas.read_csv(fpath)
-#       data = df.values
-#       print(data)
-
-#       X = data[:, :N_FEATURES]
-#       Y = data[:, N_FEATURES:]
-#       model.fit(X, Y)
-
-#   except BaseException as e:
-#     return {'type': 'append_instance',
-#             'message': f'unable to fit model: {e}'}
-
-#   return {'type': 'append_instance',
-#           'message': f'fitted model successfully'}
 
 
 def score():
@@ -239,43 +169,39 @@ def handleRequests(reqQ, respQ):
     ##############################
     ## REGION: CONTEXT SETUP    ##
     ##############################
-    print('Setting up tensorflow...')
-    
+    print('Setting up neural model...')
+
     # This stuff just exists to get the project working
     data = pd.read_csv('data/walk00_raw_and_deltas.csv').values
     N_FEATURES = 20
     N_LABELS = 4
-    
+
     context = DevContext()
     context.n_features = N_FEATURES
     context.n_labels = N_LABELS
-    
+
     # Build the data imputer
     context.imputer = IterativeImputer(max_iter=10).fit(data[:, :N_FEATURES])
     
+    # Build the data normalizer
+    context.normalizer = Normalizer().fit(data[:, :N_FEATURES])
+
     # Prepare the data
     X, y = dutils.prepare_data_imputed(data[:, :N_FEATURES], data[:, N_FEATURES:])
     X_train, y_train = X, y
-    
-    # Build the model
-    context.model = tf.keras.models.Sequential([
-        tf.keras.Input(shape=(N_FEATURES,), name="input"),
-        tf.keras.layers.LayerNormalization(axis=1 , center=True , scale=True),
-        tf.keras.layers.Dense(48, activation='relu', name='hidden_dense_1'),
-        tf.keras.layers.Dense(64, activation='relu', name='hidden_dense_2'),
-        tf.keras.layers.Dense(N_LABELS, activation='softmax', name='output')
-    ])
 
-    # Compile the model
-    context.model.compile(
-        loss='mean_squared_error',
-        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001)
+    # Build the model
+    context.model = MLPRegressor(
+        hidden_layer_sizes=(32, 42,),
+        activation='relu',
+        solver='adam',
     )
 
     # Fit the model
-    context.model.fit(X_train, y_train, epochs=6)
+    print('Fitting neural model...')
+    context.model.fit(X_train, y_train)
 
-    print('Tensorflow setup done!')
+    print('Neural model setup done!')
     ##################################
     ## END_REGION: CONTEXT SETUP    ##
     ##################################
